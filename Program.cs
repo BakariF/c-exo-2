a870b01530a7c1bb18c5f7ff5d445dbb64491c00
BakariF <bakkfofhit@live.fr>
2021-01-15 14:03:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bigfoot = 12;
+            int secondBigfoot = 25;
+            int thirdBigfoot = 6;
+            Console.WriteLine((bigfoot + secondBigfoot) * thirdBigfoot);
+        }
+    }
+}
